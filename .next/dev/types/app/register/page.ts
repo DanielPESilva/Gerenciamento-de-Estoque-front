e9eed040d7a00006b548e2151c207380ff4b711e
DanielPESilva <DanielSilDev@proton.me>
2025-11-19 19:30:40 -0400

    tela finalizada

--- .next/dev/types/app/register/page.ts
+++ .next/dev/types/app/register/page.ts
@@ -1,4 +1,4 @@
-// File: /home/superaluno/Área de trabalho/Gerenciamento-de-Estoque-Full/Gerenciamento-de-Estoque-front/src/app/register/page.tsx
+// File: /home/superaluno/Gerenciamento-de-Estoque-Full/Gerenciamento-de-Estoque-front/src/app/register/page.tsx
 import * as entry from '../../../../../src/app/register/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
